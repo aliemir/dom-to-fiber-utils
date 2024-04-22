f2b1fe9de566320b6c757493b7c8f78e755375d4
Ali Emir Şen <senaliemir@gmail.com>
2024-04-22 18:00:59 +0300

    chore(example): expose utils in window

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import React from "react";
+import * as DomToFiberUtils from "@aliemir/dom-to-fiber-utils";
 import { PointerSelector } from "./pointer-selector";
 import { FirstComponent } from "./first";
 import { SecondComponent } from "./second";
 
 function App() {
+  React.useEffect(() => {
+    // @ts-ignore - This is just for the demo
+    window.DomToFiberUtils = DomToFiberUtils;
+  }, []);
+
   return (
     <>
       <div>
